22c1583c8ff79bb13ae76df2d9e1c2902aa6fbff
Isaiah <116988172+igvim@users.noreply.github.com>
2022-11-22 18:06:21 -0500

    added function for one game round

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
+//let playerSelection = prompt('Rock, Paper, Scissors, and shoot:');
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3 + 1);
     switch (choice){
         case 1:
-            return 'Rock';
+            return 'rock';
             break;
         case 2:
-            return 'Paper';
+            return 'paper';
             break;
         case 3:
-            return 'Scissors';
+            return 'scissors';
             break;
     }
 }
 
-console.log(getComputerChoice())
+//let computerSelection = getComputerChoice();
+
+function oneRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection){
+        return 'Draw! Play again?';
+    }
+    else if (playerSelection === 'rock'){
+        if (computerSelection === 'scissors'){
+            return 'You win! Rock beats scissors'
+        }
+        else return 'You lose! Paper covers rock'
+    }
+    else if (playerSelection === 'paper'){
+        if (computerSelection === 'rock'){
+            return 'You win! Paper covers rock'
+        }
+        else return 'You lose! Scissors cuts paper'
+    }
+    else if (playerSelection === 'scissors'){
+        if (computerSelection === 'rock'){
+            return 'You lose! Rock beats scissors'
+        }
+        else return 'You win! Scissors cuts paper'
+    }
+    else return 'Huh? Enter rock, paper, or scissors'
+    }
+
+console.log(oneRound('rOck', getComputerChoice()))
