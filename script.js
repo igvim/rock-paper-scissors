1b1df26b2048b051a4a28cd2383f2ba9503aa6ad
Isaiah <116988172+igvim@users.noreply.github.com>
2022-12-19 15:03:51 -0500

    Round totals and game result work, though brittle

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ next:
 refactor oneRound to update DOM instead of console
 */
 
-const choices = document.querySelectorAll('button');
+const choices = document.querySelectorAll(".choice");
+const choiceContainer = document.querySelector(".choiceContainer");
+const roundResult = document.createElement("div");
+const runningTotal = document.createElement("div");
+const gameOver = document.createElement("div");
+let wins = 0;
+let losses = 0;
+let draws = 0;
 
 choices.forEach(choice => {
-    choice.addEventListener('click', (e) => {
-        const playerSelection = e.target.className;
-        oneRound(playerSelection, getComputerChoice());
+    choice.addEventListener("click", (e) => {
+        const playerSelection = e.target.id;
+        let oneResult = oneRound(playerSelection, getComputerChoice());
+        if (oneResult === 0) draws++;
+        else if (oneResult > 0) wins++;
+        else if (oneResult < 0) losses++;
+        runningTotal.textContent = `W: ${wins} L: ${losses} D: ${draws}`
+        choiceContainer.appendChild(runningTotal);
+        if (wins === 5 || losses === 5) {
+            let resultString = wins > losses ? `That's 5 wins! You're the champion!` : `That's 5 losses. Computer wins, pal`;
+            gameOver.textContent = resultString;
+            choiceContainer.appendChild(gameOver);
+        }
     })
 });
 
@@ -21,13 +38,13 @@ function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3 + 1);
     switch (choice){
         case 1:
-            return 'rock';
+            return "rock";
             break;
         case 2:
-            return 'paper';
+            return "paper";
             break;
         case 3:
-            return 'scissors';
+            return "scissors";
             break;
     }
 }
@@ -35,42 +52,45 @@ function getComputerChoice(){
 function oneRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection){
-        console.log('Draw! Play again?')
-        return false;
+        roundResult.textContent = "Draw! Play again!";
+        choiceContainer.appendChild(roundResult);
+        return 0;
     }
-    else if (playerSelection === 'rock'){
-        if (computerSelection === 'scissors'){
-            console.log('You win! Rock beats scissors')
-            return true;
+    else if (playerSelection === "rock"){
+        if (computerSelection === "scissors"){
+            roundResult.textContent = "You win! Rock beats scissors";
+            choiceContainer.appendChild(roundResult);
+            return 1;
         }
         else{
-            console.log('You lose! Paper covers rock')
-            return false;
+            roundResult.textContent = "You lose! Paper covers rock";
+            choiceContainer.appendChild(roundResult);
+            return -1;
         }
     }
-    else if (playerSelection === 'paper'){
-        if (computerSelection === 'rock'){
-            console.log('You win! Paper covers rock')
-            return true;
+    else if (playerSelection === "paper"){
+        if (computerSelection === "rock"){
+            roundResult.textContent = "You win! Paper covers rock";
+            choiceContainer.appendChild(roundResult);
+            return 1;
         }
         else{
-            console.log('You lose! Scissors cuts paper')
-            return false;
+            roundResult.textContent = "You lose! Scissors cuts paper";
+            choiceContainer.appendChild(roundResult);
+            return -1;
         } 
     }
-    else if (playerSelection === 'scissors'){
-        if (computerSelection === 'rock'){
-            console.log('You lose! Rock beats scissors')
-            return false;
+    else if (playerSelection === "scissors"){
+        if (computerSelection === "rock"){
+            roundResult.textContent = "You lose! Rock beats scissors";
+            choiceContainer.appendChild(roundResult);
+            return -1;
         }
         else{
-            console.log('You win! Scissors cuts paper')
-            return true;
+            roundResult.textContent = "You win! Scissors cuts paper";
+            choiceContainer.appendChild(roundResult);
+            return 1;
         }
-    }
-    else{
-        console.log('Huh? Enter rock, paper, or scissors')
-        return false;
     }
 }
 
@@ -78,13 +98,13 @@ function oneRound(playerSelection, computerSelection){
 function game(){
     let playerWins = 0;
     for (let i = 0; i < 5; i++){
-        let playerSelection = prompt('Rock, Paper, Scissors, and shoot:');
+        let playerSelection = prompt("Rock, Paper, Scissors, and shoot:");
         let winner = oneRound(playerSelection, getComputerChoice());
         if (winner) playerWins++;
     }
     playerWins >= 3 
     ? console.log(`You won ${playerWins} rounds! You win!`) 
-    : console.log(`You won ${playerWins} round(s). You didn't win.`)
+    : console.log(`You won ${playerWins} round(s). You didn"t win.`)
 }
 
 game();
